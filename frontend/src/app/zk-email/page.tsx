9257ba0d0b94845bcfb32347f13e1d0d96358419
lucholeonel <carrenoluciano@gmail.com>
2025-05-06 02:13:14 -0300

    fix loading

--- frontend/src/app/zk-email/page.tsx
+++ frontend/src/app/zk-email/page.tsx
@@ -3,7 +3,12 @@ import InboxPage from './client';
 
 export default function Page() {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={
+      <div className="min-h-screen flex flex-col justify-center items-center text-blue-600">
+        <div className="animate-spin rounded-full h-12 w-12 border-4 border-blue-300 border-t-transparent"></div>
+        <p className="mt-4 text-lg font-medium">Loading...</p>
+      </div>
+    }>
       <InboxPage />
     </Suspense>
   );
